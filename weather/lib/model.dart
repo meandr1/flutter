8e23d8703897b69c66d4182d52ea660ff993e535
Andrii Moiseienko <moyseyenko.av@gmail.com>
2023-01-11 18:12:42 +0200

    added json generator

--- weather/lib/model.dart
+++ weather/lib/model.dart
@@ -1,48 +1,67 @@
 import 'package:intl/intl.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'model.g.dart';
 
 class Weather {
   String? cityName;
-  int? temperature;
-  String? iconCode;
-  String? description;
-  DateTime? time;
+  int? temp;
+  String? icon;
+  String? main;
 
-  Weather(
-      [this.cityName,
-      this.temperature,
-      this.iconCode,
-      this.description,
-      this.time]);
+  Weather([this.cityName, this.temp, this.icon, this.main]);
 }
 
 class FormattedDate {
-  String formatDate(DateTime date) {
-    return "${DateFormat('EEEE').format(date)}, ${date.day} of ${DateFormat('MMMM').format(date)}";
+  String formatDate(String date) {
+    DateTime dateObj = DateTime.parse(date);
+    return "${DateFormat('EEEE').format(dateObj)}, ${dateObj.day} of ${DateFormat('MMMM').format(dateObj)}";
   }
 }
 
 class ExtendedWeather extends Weather with FormattedDate {
-  int? feelsLike;
-  String? dateString;
+  int? feels_like;
+  String? formattedDate;
   ExtendedWeather(
       [super.cityName,
-      super.temperature,
-      super.iconCode,
-      super.description,
-      super.time,
-      this.feelsLike,
-      this.dateString]);
+      super.temp,
+      this.feels_like,
+      super.icon,
+      super.main,
+      this.formattedDate]);
 
-  ExtendedWeather.fromJson(jsonData, DateTime date, int position) {
-    final weatherList = jsonData['list'];
+  ExtendedWeather.fromJson(jsonData, String cityName) {
+    final Temperature temperature = Temperature.fromJson(jsonData['main']);
+    final Description description =
+        Description.fromJson(jsonData['weather'][0]);
+    this.cityName = cityName;
+    temp = temperature.temp.round();
+    feels_like = temperature.feels_like.round();
+    icon = description.icon;
+    main = description.main;
+    formattedDate = formatDate(jsonData['dt_txt']);
+  }
+}
 
-    cityName = jsonData['city']['name'];
-    temperature = weatherList[position]['main']['temp'].round();
-    iconCode = weatherList[position]['weather'][0]['icon'];
-    description = weatherList[position]['weather'][0]['main'];
-    time = DateTime.parse(weatherList[position]['dt_txt']);
-    feelsLike = weatherList[position]['main']['feels_like'].round();
-    dateString = formatDate(date);
+@JsonSerializable()
+class Temperature {
+  final double temp;
+  final double feels_like;
+  Temperature(this.temp, this.feels_like);
+
+  factory Temperature.fromJson(json) {
+    return Temperature(json['temp'], json['feels_like']);
+  }
+}
+
+@JsonSerializable()
+class Description {
+  final String icon;
+  final String main;
+  Description(this.icon, this.main);
+
+  factory Description.fromJson(json) {
+    return Description(json['icon'], json['main']);
   }
 }
 
